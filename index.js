3c20ff022cfc9af95f02c0887238617ab7a89df8
Laura Lugo <lauri.lugo7@gmail.com>
2023-07-12 20:48:56 -0500

    Pseudocódigo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
@@ -7,14 +7,19 @@ const mdLinks = (path, options) => {
       //Chequear o convertir a una ruta absoluta
       //Probar si esa ruta absoluta es un archivo o un directorio
       //Si es un directorio filtrar los archivos md
-    }else{
+      //Si no hay archivos md rechazar la promesa
+      //Si hay archivos md leerlos
+      //Si no hay links rechazar la promesa
+      //Si hay links guardarlos en un array de objetos
+      //Si no hay opciones resolver la promesa con el array de objetos
+      //Si hay opciones comprobar los enlaces y guardarlos en un array de objetos y resolver la promesa con el array de objetos
+    } else {
       //Si no existe la ruta se rechaza la promesa
-      reject('La ruta no existe');
+      reject("La ruta no existe");
     }
   });
-};  
-
+};
 
 module.exports = {
-  mdLinks
+  mdLinks,
 };
