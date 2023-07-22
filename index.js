ef83a215380a5af38a30928d4622348b647bb520
Laura Lugo <lauri.lugo7@gmail.com>
2023-07-22 09:38:30 -0500

    Comentarios

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require("fs");
 
 const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
+    //Si la ruta/path está vacía mostrar error
+    //Chequear o convertir a una ruta absoluta
     //Identifica si la ruta existe
     if (fs.existsSync(path)) {
-      //Chequear o convertir a una ruta absoluta
       //Probar si esa ruta absoluta es un archivo o un directorio
       //Si es un directorio filtrar los archivos md
       //Si no hay archivos md rechazar la promesa
