14816173c35bf989b3acc509ed4f401bf1f477e8
Laura Lugo <lauri.lugo7@gmail.com>
2023-08-06 18:02:55 -0500

    Se corrige Options y Recursividad

--- index.js
+++ index.js
@@ -1,54 +1,70 @@
 const fs = require("fs");
+const { existsSync, statSync, readdirSync } = fs;
+const path = require("path");
+const _resolve = path.resolve;
+const calculate = require("./calculate.js");
 //Obtiene la ruta y las opciones
 const mdLinks = (path, options) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(function (resolve, reject) {
+    //si no hay opciones dejarlas en false por el test
+    if (options === undefined) {
+      options = {
+        validate: false,
+        stats: false,
+      };
+    }
+
     //Si la ruta/path está vacía mostrar error
-    if (path === "") {
+    if (path === "/" || path === undefined) {
       reject("La ruta está vacía");
     }
     //Chequear o convertir a una ruta absoluta
     //Si la ruta es relativa convertirla a absoluta
     if (path[0] !== "/") {
-      path = path.resolve(path);
+      path = _resolve(path);
     }
     //Identifica si la ruta existe
-    if (fs.existsSync(path)) {
+    if (existsSync(path)) {
       //Si la ruta es un archivo
-      if (fs.statSync(path).isFile()) {
+      if (statSync(path).isFile()) {
         // ¿el archivo es un .md?
         if (path.slice(-3) === ".md") {
           //se agrega a un arreglo de archivos
           let files = [];
           files.push(path);
           calculate(files, resolve, reject, options);
-        }
-        else {
+        } else {
           reject("No es un archivo .md");
         }
-
       } else {
         // filtrar todos los archivos .md
         let files = [];
-        const readDir = (path) => {
-          //Lee el directorio
-          const filesAndDirs = fs.readdirSync(path);
-          //Recorre el directorio
-          filesAndDirs.forEach((fileOrDir) => {
+        const leerCarpeta = (ruta) => {
+          //Lee la carpeta
+          const archivosYCarpetas = readdirSync(ruta);
+          //Recorre las carpetas y archivos
+          for (const archivoOCarpeta of archivosYCarpetas) {
             //Si es un archivo .md se agrega al arreglo de archivos
-            if (fileOrDir.slice(-3) === ".md") {
-              files.push(path + "/" + fileOrDir);
+            if (archivoOCarpeta.slice(-3) === ".md") {
+              files.push(ruta + "/" + archivoOCarpeta);
             } else {
-              //Si es un directorio se vuelve a llamar la función
-              readDir(path + "/" + fileOrDir);
+              //Si es una carpeta
+              if (statSync(ruta + "/" + archivoOCarpeta).isDirectory()) {
+                //se lee el contenido de la carpeta
+                leerCarpeta(ruta + "/" + archivoOCarpeta);
+              }
             }
           }
-          );
-        }
+        };
+
+        //Se llama a la función leerCarpeta
+        leerCarpeta(path);
+
         //¿Existen archivos .md?
         if (files.length === 0) {
           reject("No hay archivos .md");
         }
-        //Si existen archivos .md se agregan al arreglo de archivos(línea 30)
+        //Si existen archivos .md se agregan al arreglo de archivos(línea 49)
         calculate(files, resolve, reject, options);
       }
     } else {
@@ -57,7 +73,4 @@ const mdLinks = (path, options) => {
     }
   });
 };
-
-module.exports = {
-  mdLinks,
-};
+module.exports = mdLinks;
