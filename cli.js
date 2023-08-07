650c46eea869fc5058fb8c6c4e0e9c58c6191e8f
Laura Lugo <lauri.lugo7@gmail.com>
2023-08-06 19:28:50 -0500

    CLI y Test

--- cli.js
+++ cli.js
@@ -1,8 +1,42 @@
-const { mdLinks } = require("./index.js");
-mdLinks("/noexiste/")
-  .then((res) => {
-    console.log(res);
+#!/usr/bin/env node
+const { program } = require("commander");
+const mdLinks = require("./index.js");
+
+// Definir las opciones de CLI
+program
+  .option("-v, --validate", "Validate the links")
+  .option("-s, --stats", "Show statistics about the links")
+  .parse(process.argv);
+
+// Obtener la ruta de CLI
+const path = program.args[0];
+// Obtener las opciones de CLI
+const options = program.opts();
+
+// Llamar a la función mdLinks con la ruta y las opciones 
+mdLinks(path, { validate: options.validate, stats: options.stats})
+  .then((result) => {
+    if (Array.isArray(result)) {
+      if (options.validate) {
+        for (const link of result) {
+          console.log(
+            `${link.file} ${link.href} ${link.ok} ${link.status} ${link.text}`
+          );
+        }
+      } else {
+        for (const link of result) {
+          console.log(`${link.file} ${link.href} ${link.text}`);
+        }
+      }
+    } else {
+      for (const key of Object.keys(result)) {
+        console.log(`${key}: ${result[key]}`);
+      }
+    }
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    console.log("Gracias por usar md-links ❤️");
   });
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,18 +1,33 @@
-const { mdLinks } = require('../index.js');
+const mdLinks = require("../index.js");
 
+describe("mdLinks", () => {
+  it("RUTA ERRONEA: Debe rechazar cuando el path no existe", () => {
+    return mdLinks("/noexiste/").catch((error) => {
+      expect(error).toBe("La ruta no existe");
+    });
+  });
 
-describe('mdLinks', () => {
+  it("ARCHIVO: Debe devolver un array de un objeto", () => {
+    return mdLinks("unlink.md").then((result) => {
+      expect(result.length == 1).toBe(true);
+    });
+  });
 
-  // it('should...', () => {
-  //   console.log('FIX ME!');
-  // });
+  it("ARCHIVO (validate): Debe devolver un array con validaciones y el primero da status 200", () => {
+    return mdLinks("unlink.md",{validate:true}).then((result) => {
+      expect(result[0]["status"] == 200).toBe(true);
+    });
+  });
 
+  it("ARCHIVO (validate y stats): Debe devolver un objeto", () => {
+    return mdLinks("unlink.md",{validate:true, stats:true}).then((result) => {
+      expect(Array.isArray(result)).toBe(false);
+    });
+  });
 
-
-  it('Debe rechazar cuando el path no existe', () => {
-    return mdLinks('/noexiste/')
-      .catch((error) => {
-        expect(error).toBe('La ruta no existe');
-      });
+  it("CARPETA: Debe ser un array con más de un objeto de respuesta", () => {
+    return mdLinks("./").then((result) => {
+      expect(result.length > 1).toBe(true);
+    });
   });
 });
